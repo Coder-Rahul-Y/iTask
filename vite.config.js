911e7a76cb30d7db3b87dbe120f24df42813cf6e
Rahul Yadav <hulracoc12345@gmail.com>
2025-08-24 16:31:54 +0530

    used env vars in vite.config.js for detecting deployment platform

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const isVercel = process.env.DEPLOY_ENV === 'VERCEL'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
-  base:"/iTask/"
+  base: isVercel? "/" : "/iTask/",
 })
